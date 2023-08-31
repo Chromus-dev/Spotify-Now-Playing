713c93db5916f510fb5363067ae17436de591836
Chromus-dev <xlchannel6@gmail.com>
2023-08-31 12:09:09 -0700

    Initial Commit

---
+++ Spotify Now Playing/Spotify_Now_PlayingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Spotify_Now_PlayingApp.swift
+//  Spotify Now Playing
+//
+//  Created by Chase Crampton on 8/31/23.
+//
+
+import SwiftUI
+
+@main
+struct Spotify_Now_PlayingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
